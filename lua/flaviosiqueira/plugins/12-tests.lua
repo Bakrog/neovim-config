d779bd37e0cf0ad59186e04fe8af68b47f2b47c0
Flavio Prado <flavio.prado1@paxful.com>
2024-11-07 19:29:09 +0200

    Add test.lua

---
+++ lua/flaviosiqueira/plugins/12-tests.lua
@@ -0,0 +1,46 @@
+return {
+    "nvim-neotest/neotest",
+    dependencies = {
+        "nvim-neotest/nvim-nio",
+        "nvim-lua/plenary.nvim",
+        "antoinemadec/FixCursorHold.nvim",
+        "nvim-treesitter/nvim-treesitter",
+        "nvim-neotest/neotest-plenary",
+        "nvim-neotest/neotest-python",
+        "nvim-neotest/neotest-jest",
+        "jfpedroza/neotest-elixir",
+        "rouge8/neotest-rust",
+    },
+    config = function()
+        local neotest = require("neotest")
+        neotest.setup({
+            adapters = {
+                require("neotest-plenary").setup({
+                    -- this is my standard location for minimal vim rc
+                    -- in all my projects
+                    min_init = "./scripts/tests/minimal.vim",
+                }),
+                require("neotest-python"),
+                require("neotest-jest")({
+                    jestCommand = "npm test --",
+                    jestConfigFile = "custom.jest.config.ts",
+                    env = { CI = true },
+                    cwd = function()
+                        return vim.fn.getcwd()
+                    end,
+                }),
+                require("neotest-elixir"),
+                require("neotest-rust"),
+            }
+        })
+
+        vim.keymap.set("n", "<leader>tc", function()
+            neotest.run.run()
+        end)
+
+        vim.keymap.set("n", "<leader>tf", function()
+            neotest.run.run(vim.fn.expand("%"))
+        end)
+    end,
+}
+
