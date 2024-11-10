5c1e90d5f7b27f0d159f8a19f1ecfda515ee1543
Flavio Prado <flavio.prado1@paxful.com>
2024-11-10 05:33:45 +0200

    fix shortcuts

--- lua/flaviosiqueira/plugins/09-vim-fugitive.lua
+++ lua/flaviosiqueira/plugins/09-vim-fugitive.lua
@@ -2,7 +2,7 @@ return {
     "tpope/vim-fugitive",
 
     config = function ()
-        vim.keymap.set("n", "<leader>gs", function ()
+        vim.keymap.set("n", "<leader>ss", function ()
             local git_command = vim.fn.input("> ")
             vim.cmd.Git(git_command)
         end)
