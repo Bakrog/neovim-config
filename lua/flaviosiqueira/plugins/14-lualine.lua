8fc17658ed92f8895d10ad7d3ee37fd53b4028fc
Flavio Prado <flavio.s.prado@mail.com>
2025-03-11 07:19:36 +0200

    feat: add noice

--- lua/flaviosiqueira/plugins/14-lualine.lua
+++ lua/flaviosiqueira/plugins/14-lualine.lua
@@ -1,8 +1,9 @@
 return {
     "nvim-lualine/lualine.nvim",
     requires = { "nvim-tree/nvim-web-devicons" },
+    dependencies = { "folke/noice.nvim" },
 
-    config = function ()
+    config = function()
         require("lualine").setup({
             options = {
                 theme = "rose-pine",
@@ -13,7 +14,32 @@ return {
                 lualine_a = { "mode" },
                 lualine_b = { "branch" },
                 lualine_c = { "filename" },
-                lualine_x = { "encoding", "fileformat", "filetype" },
+                lualine_j = {
+                    {
+                        require("noice").api.status.message.get_hl,
+                        cond = require("noice").api.status.message.has,
+                    },
+                    {
+                        require("noice").api.status.command.get,
+                        cond = require("noice").api.status.command.has,
+                        color = { fg = "#ff9e64" },
+                    },
+                    {
+                        require("noice").api.status.mode.get,
+                        cond = require("noice").api.status.mode.has,
+                        color = { fg = "#ff9e64" },
+                    },
+                    {
+                        require("noice").api.status.search.get,
+                        cond = require("noice").api.status.search.has,
+                        color = { fg = "#ff9e64" },
+                    },
+                },
+                lualine_x = {
+                    "encoding",
+                    "fileformat",
+                    "filetype",
+                },
                 lualine_y = { "progress" },
                 lualine_z = { "location" },
             },
@@ -37,4 +63,3 @@ return {
         })
     end
 }
-
--- lua/flaviosiqueira/plugins/16-nvim-cmp.lua
+++ lua/flaviosiqueira/plugins/16-nvim-cmp.lua
@@ -15,6 +15,13 @@ return {
     {
         "folke/noice.nvim",
         event = "VeryLazy",
+        keys = {
+            {
+                "nh",
+                "<cmd>Noice telescope<cr>",
+                mode = { "n" },
+            },
+        },
         opts = {
             lsp = {
                 -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
@@ -30,7 +37,7 @@ return {
                 command_palette = true, -- position the cmdline and popupmenu together
                 long_message_to_split = true, -- long messages will be sent to a split
                 inc_rename = false, -- enables an input dialog for inc-rename.nvim
-                lsp_doc_border = false, -- add a border to hover docs and signature help
+                lsp_doc_border = true, -- add a border to hover docs and signature help
             },
         },
         dependencies = {
